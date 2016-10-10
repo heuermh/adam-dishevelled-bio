da9d8744d9059f593ac4ade00a8a2a2c2a380e2c
Michael L Heuer <heuermh@acm.org>
2016-10-10 10:16:30 -0500

    first checked in

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # adam-dishevelled-bio
+
+###Hacking adam-dishevelled-bio
+
+Install
+
+ * JDK 1.8 or later, http://openjdk.java.net
+ * Apache Maven 3.3.9 or later, http://maven.apache.org
+ * dishevelled-bio, http://github.com/heuermh/dishevelled-bio
+
+To build
+
+    $ mvn install
+
+###Running adam-dishevelled-bio using `spark-submit`
+
+```bash
+$ spark-submit \
+    --class com.github.heuermh.adam.dishevelledbio.AdamDshFilterVcf \
+    target/adam-dishevelled-bio_2.10-0.19.1-SNAPSHOT.jar \
+    --id rs201888535 \
+    --single \
+    input.vcf \
+    output.vcf
+```
---
+++ src/main/scala/com/github/heuermh/adam/dishevelledbio/AdamDshFilterVcf.scala
@@ -0,0 +1,92 @@
+/*
+
+    adam-dishevelled-bio  dishevelled-bio on Spark via ADAM pipe APIs.
+    Copyright (c) 2016 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package com.github.heuermh.adam.dishevelledbio
+
+import org.apache.spark.SparkContext
+import org.bdgenomics.adam.models.VariantContext
+import org.bdgenomics.adam.rdd.ADAMContext._
+import org.bdgenomics.adam.rdd.variation.{ VariantContextRDD, VCFInFormatter, VCFOutFormatter }
+import org.bdgenomics.utils.cli._
+import org.bdgenomics.utils.misc.Logging
+import org.kohsuke.args4j.{ Option => Args4jOption, Argument }
+
+class AdamDshFilterVcfArgs extends ParquetArgs {
+  @Argument(required = true, metaVar = "INPUT", usage = "input VCF file", index = 0)
+  var inputVcfFile: String = _
+
+  @Argument(required = true, metaVar = "OUTPUT", usage = "output VCF file", index = 1)
+  var outputVcfFile: String = null
+
+  @Args4jOption(required = false, name = "--id", usage = "filter by id, specify as id1,id2,id3")
+  var id: String = null
+
+  @Args4jOption(required = false, name = "--range", usage = "filter by range, specify as chrom:start-end in 0-based coordinates")
+  var range: String = null
+
+  @Args4jOption(required = false, name = "--qual", usage = "filter by quality score")
+  var qual: String = null
+
+  @Args4jOption(required = false, name =" --filter", usage = "filter to records that have passed all filters")
+  var filter: String = null
+
+  @Args4jOption(required = false, name = "--single", usage = "save OUTPUT as single file, default false")
+  var asSingleFile: Boolean = false
+}
+
+object AdamDshFilterVcf extends BDGCommandCompanion {
+  override val commandName = "adam-dsh-filter-vcf"
+  override val commandDescription = "dsh-filter-vcf on Spark via ADAM pipe APIs"
+
+  override def apply(cmdLine: Array[String]): AdamDshFilterVcf =
+    new AdamDshFilterVcf(Args4j[AdamDshFilterVcfArgs](cmdLine))
+}
+
+/**
+ * dsh-filter-vcf on Spark via ADAM pipe APIs.
+ * 
+ * @author  Michael Heuer
+  */
+class AdamDshFilterVcf(val args: AdamDshFilterVcfArgs) extends BDGSparkCommand[AdamDshFilterVcfArgs] with Logging {
+  override val companion = AdamDshFilterVcf
+
+  override def run(sc: SparkContext): Unit = {
+    val input: VariantContextRDD = sc.loadVcf(args.inputVcfFile)
+
+    implicit val tFormatter = VCFInFormatter
+    implicit val uFormatter = new VCFOutFormatter
+
+    // reassemble command from our arguments
+    val dshFilterVcfCommand = StringBuilder.newBuilder
+    dshFilterVcfCommand.append("dsh-filter-vcf")
+    Option(args.id).foreach(id => dshFilterVcfCommand.append(" --id " + id))
+    Option(args.range).foreach(range => dshFilterVcfCommand.append(" --range " + range))
+    Option(args.qual).foreach(qual => dshFilterVcfCommand.append(" --qual " + qual))
+    Option(args.filter).foreach(filter => dshFilterVcfCommand.append(" --filter " + filter))
+
+    val output: VariantContextRDD = input.pipe[VariantContext, VariantContextRDD, VCFInFormatter](dshFilterVcfCommand.toString)
+      .transform(_.cache())
+
+    output.saveAsVcf(args.outputVcfFile, args.asSingleFile)
+  }
+}
